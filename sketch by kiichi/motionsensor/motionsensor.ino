1bf3704752cb909d2f1f671cd0d332d33f5ec1db
ki-chi <mrll.rumizu.0304@gmail.com>
2016-09-27 17:30:29 +0900

    モーションセンサのスケッチ文

---
+++ sketch by kiichi/motionsensor/motionsensor.ino
@@ -0,0 +1,31 @@
+#include <Console.h>
+#include <Bridge.h>
+
+const int LED = 13;
+const int PIR = 2;
+unsigned long start_t = 0;
+unsigned long end_t;
+
+void setup() { 
+  Bridge.begin();
+  Console.begin();
+  pinMode(LED, OUTPUT);
+  pinMode(PIR, INPUT);
+}
+ 
+void loop() {
+  int aa = digitalRead(PIR) ;
+  Console.println(aa) ;
+  if (digitalRead(PIR) == HIGH) {
+    digitalWrite(LED, HIGH);
+    start_t = millis();
+  } 
+  else {
+    end_t = millis();
+    if(end_t - start_t >= 10000){
+      digitalWrite(LED, LOW);
+    }
+  }
+ 
+  delay(200);
+}
